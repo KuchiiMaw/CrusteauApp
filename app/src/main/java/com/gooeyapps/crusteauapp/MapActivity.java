8d260d4c9f04f44b6e4d231bde1c27c69dea3294
jaumebf <38112131+jaumebf@users.noreply.github.com>
2018-04-13 17:50:19 +0200

    Mapa

--- app/src/main/java/com/gooeyapps/crusteauapp/MapActivity.java
+++ app/src/main/java/com/gooeyapps/crusteauapp/MapActivity.java
@@ -2,11 +2,11 @@ package com.gooeyapps.crusteauapp;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -38,9 +38,18 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        final LatLng coordenades = new LatLng(46.325132, 1.269627);
+        MarkerOptions marker = new MarkerOptions().position(coordenades).title("Crusteau");
+        mMap.addMarker(marker);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(coordenades));
+
+        mMap.setOnMapLoadedCallback(new GoogleMap.OnMapLoadedCallback() {
+            public void onMapLoaded() {
+                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(new CameraPosition.Builder()
+                        .target(coordenades)
+                        .zoom(7)
+                        .build()));
+            }
+        });
     }
 }
