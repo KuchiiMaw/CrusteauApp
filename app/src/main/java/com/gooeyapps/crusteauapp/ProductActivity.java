3487eecda7e382a151add3f2d7eab0a861594e29
jaumebf <38112131+jaumebf@users.noreply.github.com>
2018-04-25 17:34:17 +0200

    Afegir foto individual de cada producte al clicar

--- app/src/main/java/com/gooeyapps/crusteauapp/ProductActivity.java
+++ app/src/main/java/com/gooeyapps/crusteauapp/ProductActivity.java
@@ -46,7 +46,55 @@ public class ProductActivity extends AppCompatActivity {
                 descripcio.setText("La coca integral a la Jose de ");
                 break;
 
+            case 3:
+                imageView.setImageResource(R.drawable.croissant);
+                descripcio.setText("Croassant");
+                break;
 
+            case 4:
+                imageView.setImageResource(R.drawable.espelta_integral);
+                descripcio.setText("Pa d'espelta");
+                break;
+
+            case 5:
+                imageView.setImageResource(R.drawable.fajol);
+                descripcio.setText("Fajol");
+                break;
+
+            case 6:
+                imageView.setImageResource(R.drawable.magdalena);
+                descripcio.setText("Magdalena");
+                break;
+
+            case 7:
+                imageView.setImageResource(R.drawable.pa_moresc);
+                descripcio.setText("Pa moresc");
+                break;
+
+            case 8:
+                imageView.setImageResource(R.drawable.pa_motlle);
+                descripcio.setText("Pa de motlle");
+                break;
+
+            case 9:
+                imageView.setImageResource(R.drawable.pages);
+                descripcio.setText("Pa de pagès");
+                break;
+
+            case 10:
+                imageView.setImageResource(R.drawable.rustic);
+                descripcio.setText("Pa rústic");
+                break;
+
+            case 11:
+                imageView.setImageResource(R.drawable.segol_integral);
+                descripcio.setText("Pa de sègol integral");
+                break;
+
+            case 12:
+                imageView.setImageResource(R.drawable.sis_cereals);
+                descripcio.setText("Pa 6 cereals");
+                break;
         }
 
 
