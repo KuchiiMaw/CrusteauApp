9f5aa2c51b95c3655937114f299b9d1ecc0ae27b
jaumebf <38112131+jaumebf@users.noreply.github.com>
2018-05-04 16:39:15 +0200

    Mida tablet MainActivity i visualment be

--- app/src/main/java/com/gooeyapps/crusteauapp/MainActivity.java
+++ app/src/main/java/com/gooeyapps/crusteauapp/MainActivity.java
@@ -22,7 +22,6 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);//No permetre girar el Layout
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
